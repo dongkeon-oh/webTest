91766d991f293db73fb3e07d3838a09b06eb16a3
Dongkeon Oh <dongkeon.oh@gmail.com>
2022-07-04 14:50:23 +0900

    웹훅 테스트

--- src/main/java/com/frank/test/HomeController.java
+++ src/main/java/com/frank/test/HomeController.java
@@ -31,6 +31,8 @@ public class HomeController {
 		
 		String formattedDate = dateFormat.format(date);
 		
+		System.out.println("test");
+		
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
